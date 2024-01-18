b840779398ee99137d17971ccc2a458cc53ed19b
MathisZerbib <mathis.zerbib@gmail.com>
2024-01-18 21:43:28 +0100

    added layout + login page + earth svg + renamed folder assets

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,6 @@
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -14,7 +13,10 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 
+import Image from "next/image";
 import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import AnimatedEarth from "@/components/animatedEarth";
 
 const formSchema = z.object({
   email: z
@@ -54,45 +56,81 @@ const LoginPage = () => {
 
   return (
     <>
-      <MaxWidthWrapper>
-        <p>Se connecter</p>
-        <p>Pour continuer sur An’gova</p>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Email" {...field} />
-                  </FormControl>
-                  <FormDescription></FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+      <div className="relative">
+        <div className="w-1/2 lg:w-1/3 flex-col mx-auto flex justify-center space-y-8 h-screen">
+          <div className="w-full">
+            <p className="font-bold text-2xl text-center my-5">Se connecter</p>
+            <p className="text-center">
+              Pour continuer sur{" "}
+              <span className="text-orange-400">An’gova</span>
+            </p>
+            <div className="flex justify-center">
+              <Separator
+                orientation="horizontal"
+                className="w-1/2 my-5 bg-orange-400"
+              />
+              <div className="mx-5"></div>
+              <Separator
+                orientation="horizontal"
+                className="w-1/2 my-5 bg-orange-400"
+              />
+            </div>
+          </div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Email" {...field} />
+                    </FormControl>
+                    <FormDescription></FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input type="password" placeholder="Password" {...field} />
-                  </FormControl>
-                  <FormDescription></FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Mot de passe</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="Password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription></FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-            <Button type="submit">Connexion</Button>
-          </form>
-        </Form>
-      </MaxWidthWrapper>
+              <Button type="submit" variant="orange" className="w-full">
+                Connexion
+              </Button>
+            </form>
+          </Form>
+          <div
+            className="absolute bottom-0 left-0 w-full"
+            style={{
+              position: "absolute",
+              bottom: "0",
+              height: "120px",
+              overflow: "hidden",
+            }}
+          >
+            <AnimatedEarth />
+          </div>
+        </div>
+        {/* make sticky bottom */}
+      </div>
     </>
   );
 };
