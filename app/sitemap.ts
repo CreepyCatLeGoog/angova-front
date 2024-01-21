a76b8c2d02271436a582eab2fed06fe5d055a658
Mathis Zerbib <mathis.zerbib@epitech.eu>
2024-01-21 17:44:38 +0100

    Added stepper indicator

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -30,7 +30,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
         url: 'https://angova.io/signup',
         lastModified: new Date(),
         changeFrequency: 'weekly',
-        priority: 0.5,
+        priority: 0.3,
       }
   ]
 }
