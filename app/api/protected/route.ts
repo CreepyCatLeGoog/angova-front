659d59455ca8aea1f80cdfc225cd641534c6b2c9
Mathis Zerbib <mathis.zerbib@epitech.eu>
2024-02-11 21:59:32 +0100

    SignIn SignOut component added

--- app/api/protected/route.ts
+++ app/api/protected/route.ts
@@ -10,3 +10,18 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   }
   res.status(401).json("You must be signed in.")
 }
+
+// import { UserRole } from "@prisma/client";
+// import { NextResponse } from "next/server";
+
+// import { currentRole } from "@/lib/auth";
+
+// export async function GET() {
+//   const role = await currentRole();
+
+//   if (role === UserRole.ADMIN) {
+//     return new NextResponse(null, { status: 200 });
+//   }
+
+//   return new NextResponse(null, { status: 403 });
+// }
