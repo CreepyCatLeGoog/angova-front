18630adb0f8d17381111822dd5a048e5c4db494d
Mathis Zerbib <mathis.zerbib@epitech.eu>
2024-02-12 11:48:14 +0100

    added avatar example + login facebook + apple

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,8 +6,8 @@ import Image from "next/image";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
-import { toast } from "@/components/ui/use-toast";
 import { useToast } from "@/components/ui/use-toast";
+import { toast } from "@/components/ui/use-toast";
 import { signIn } from "next-auth/react";
 import { LoginUserInput, loginUserSchema } from "@/lib/user-schema";
 import { Button } from "@/components/ui/button";
@@ -115,6 +115,13 @@ export const LoginForm = () => {
     <>
       <div className="h-[10vh]"></div>
       <LoginHeader shouldAnimate={shouldAnimate} />
+      <LoginThirdParty
+        shouldAnimate={shouldAnimate}
+        callbackUrl={callbackUrl}
+      />
+      <div className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
+        <p className="text-center font-semibold mx-4 mb-0">OR</p>
+      </div>
       <form
         onSubmit={handleSubmit(onSubmitHandler)}
         style={{
@@ -152,16 +159,12 @@ export const LoginForm = () => {
         </div>
         <button
           type="submit"
-          style={{ backgroundColor: `${submitting ? "#ccc" : "#3446eb"}` }}
-          className="inline-block px-7 py-4 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
+          style={{ backgroundColor: `${submitting ? "#ccc" : "#F49E4C"}` }}
+          className="inline-block px-7 py-4 bg-orange-500 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-orange-700 hover:shadow-lg focus:bg-orange-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-800 active:shadow-lg transition duration-150 ease-in-out w-full"
           disabled={submitting}
         >
           {submitting ? "loading..." : "Sign In"}
         </button>
-
-        <div className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
-          <p className="text-center font-semibold mx-4 mb-0">OR</p>
-        </div>
 
         {/* <a
         className="px-7 py-2 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center"
@@ -170,7 +173,7 @@ export const LoginForm = () => {
         role="button"
       >
         <Image
-          className="pr-2"
+          className="p-1"
           src="/images/github.svg"
           alt=""
           width={40}
@@ -180,31 +183,6 @@ export const LoginForm = () => {
       </a> */}
       </form>
 
-      <form
-        style={{
-          opacity: shouldAnimate ? 0 : 1,
-        }}
-        action={async () => {
-          await signIn("google", { callbackUrl });
-        }}
-      >
-        <Button
-          className="px-7 py-2 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3"
-          variant="orange"
-          onClick={() => signIn("google", { callbackUrl })}
-          role="button"
-        >
-          <Image
-            className="pr-2"
-            src="assets/google.svg"
-            alt=""
-            style={{ height: "2rem" }}
-            width={35}
-            height={35}
-          />
-          Continue with Google
-        </Button>
-      </form>
       <div className="fixed bottom-0 left-0 right-0 mb-5 pb-20">
         <motion.div
           style={{
@@ -230,163 +208,70 @@ export const LoginForm = () => {
   );
 };
 
-// async function animateFailedForm() {
-//   setShouldAnimateFailed(true);
-// }
+const LoginThirdParty = ({
+  shouldAnimate,
+  callbackUrl,
+}: {
+  shouldAnimate: boolean;
+  callbackUrl: string;
+}) => {
+  return (
+    <div
+      className="flex flex-row gap-5 mx-2 transition duration-150 ease-in-out w-full justify-center items-center mb-3"
+      style={{
+        opacity: shouldAnimate ? 0 : 1,
+      }}
+    >
+      <Button
+        className="px-7 py-2 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3"
+        variant="outline"
+        onClick={() => signIn("apple", { callbackUrl })}
+        role="button"
+      >
+        <Image
+          className="p-1"
+          src="assets/apple.svg"
+          alt="Apple login logo"
+          style={{ height: "2rem" }}
+          width={30}
+          height={30}
+        />
+      </Button>
 
-// return (
-//   <>
-//     <div className="relative">
-//       <div className="w-1/2 lg:w-1/3 flex-col mx-auto flex justify-center items-center h-[90vh]">
-//         <LoginHeader shouldAnimate={shouldAnimate} />
-//         <motion.div
-//           initial={{ opacity: 0 }}
-//           animate={shouldAnimate ? { opacity: 1 } : {}}
-//           transition={{ duration: 1, ease: "easeInOut" }}
-//         >
-//           <p
-//             className={cn(
-//               "",
-//               `${
-//                 shouldAnimate
-//                   ? "text-center text-2xl font-bold block"
-//                   : "none"
-//               }`
-//             )}
-//           >
-//             Welcome to angova
-//           </p>
-//           <div
-//             className={(cn(""), `${shouldAnimate ? "block my-40" : "none"}`)}
-//           ></div>
-//         </motion.div>
-//         <Form {...form}>
-//           <motion.form
-//             onSubmit={handleSubmit}
-//             initial={{ opacity: 1, y: 0 }}
-//             animate={shouldAnimateFailed ? formAnimationControls : {}}
-//             transition={{ duration: 1, ease: "easeInOut" }}
-//           >
-//             <FormField
-//               name="email"
-//               render={({ field }) => (
-//                 <motion.div
-//                   initial={{ opacity: 1, scale: 1 }}
-//                   animate={shouldAnimate ? { opacity: 0, scale: 0 } : {}}
-//                   transition={{ duration: 1, ease: "easeInOut" }}
-//                 >
-//                   <FormItem>
-//                     <FormControl>
-//                       <Input placeholder="Email" {...field} />
-//                     </FormControl>
-//                     <FormDescription></FormDescription>
-//                     <FormMessage />
-//                   </FormItem>
-//                 </motion.div>
-//               )}
-//             />
-//             <div className="flex justify-center">
-//               <div className="my-2"></div>
-//             </div>
+      <Button
+        className="px-7 py-2 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3"
+        variant="outline"
+        onClick={() => signIn("facebook", { callbackUrl })}
+        role="button"
+      >
+        <Image
+          className="p-1"
+          src="assets/facebook.svg"
+          alt="Facebook login logo"
+          style={{ height: "2rem" }}
+          width={30}
+          height={30}
+        />
+      </Button>
 
-//             <FormField
-//               name="password"
-//               render={({ field }) => (
-//                 <motion.div
-//                   initial={{ opacity: 1, scale: 1 }}
-//                   animate={shouldAnimate ? { opacity: 0, scale: 0 } : {}}
-//                   transition={{ duration: 1, ease: "easeInOut" }}
-//                 >
-//                   <FormItem>
-//                     <FormControl>
-//                       <Input
-//                         type="password"
-//                         placeholder="Password"
-//                         {...field}
-//                       />
-//                     </FormControl>
-//                     <FormDescription>
-//                       <a
-//                         href="#"
-//                         className="underline underline-offset-4 font-style-italic text-xs"
-//                       >
-//                         Forgot your password ?
-//                       </a>
-//                     </FormDescription>
-//                     <FormMessage />
-//                   </FormItem>
-//                 </motion.div>
-//               )}
-//             />
-//             <div className="flex justify-center">
-//               <div className="my-5"></div>
-//             </div>
-
-//             <Button
-//               type="submit"
-//               variant="orange"
-//               className="w-full"
-//               style={{
-//                 opacity: shouldAnimate ? 0 : 1,
-//               }}
-//             >
-//               Connexion
-//             </Button>
-//             {/* /// button google  */}
-//             {/* <Button
-//               onClick={async () => {
-//                 const res = await signIn("google");
-
-//                 if (res?.error) {
-//                   setError("Invalid email or password");
-//                   await animateFailedForm();
-//                 } else if (res?.ok) {
-//                   await animateForm();
-//                 } else {
-//                   setError("An error occurred");
-//                   await animateFailedForm();
-//                 }
-//               }}
-//               className="w-full bg-white text-black border border-black rounded-md p-2 my-2"
-//             >
-//               Sign in with Google
-//             </Button> */}
-//           </motion.form>
-//         </Form>
-//         <form
-//           action={async () => {
-//             await signIn("google");
-//           }}
-//         >
-//           <button>Sign in with google</button>
-//           <br />
-//         </form>
-//         <div className="h-[20vh]"></div>
-//       </div>
-
-//       <motion.div
-//         style={{
-//           position: "absolute",
-//           // bottom: "50px",
-//           width: "100%",
-//           overflow: "hidden",
-//         }}
-//         initial="hidden"
-//         animate={shouldAnimate ? "center" : "hidden"}
-//         variants={{
-//           center: { y: -700, height: 500, scale: 0.5 },
-//           hidden: { y: -130, height: 200, scale: 1.1, overflow: "hidden" },
-//         }}
-//         transition={{ duration: 0.4, ease: "easeInOut", delay: 0.4 }}
-//       >
-//         <AnimatedEarth
-//           shouldAnimateFailed={shouldAnimateFailed}
-//           shouldAnimate={shouldAnimate}
-//         />
-//       </motion.div>
-//     </div>
-//   </>
-
+      <Button
+        className="px-7 py-2 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3"
+        variant="outline"
+        onClick={() => signIn("google", { callbackUrl })}
+        role="button"
+      >
+        <Image
+          className="p-1"
+          src="assets/google.svg"
+          alt="Google login logo"
+          style={{ height: "2rem" }}
+          width={30}
+          height={30}
+        />
+      </Button>
+    </div>
+  );
+};
 const LoginHeader = ({ shouldAnimate }: { shouldAnimate: boolean }) => {
   const headerAnimationControls = useAnimation();
 
