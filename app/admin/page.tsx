fb0a4880b8c6d00ad69d698c0a2914f67c06c025
laurent <laurent.cochonneau@gmail.com>
2024-03-21 14:17:33 +0100

    fixed modal

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,19 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper'
-import { UserModal } from '@/components/new-user'
 import { Button } from '@/components/ui/button'
-import { DialogTrigger } from '@/components/ui/dialog'
 import { LogOut, UserPlus } from 'lucide-react'
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
 
 const AdminDashboard = () => {
     return (
@@ -14,12 +25,88 @@ const AdminDashboard = () => {
                     </div>
                 </nav>
                 <div className='flex justify-around mt-6'>
+                <Dialog>
+                        <DialogTrigger asChild>
+                            <Button variant='new'>
+                                Nouvel utilisateur
+                                <UserPlus className='w-6 h-6 ml-2' />
+                            </Button>
+                        </DialogTrigger>
+                        <DialogContent className="sm:max-w-[425px]">
+                            <DialogHeader>
+                                <DialogTitle>Enregistrement</DialogTitle>
+                                <DialogDescription>
+                                    Modifiez ici les informations de l'utilisateur.
+                                </DialogDescription>
+                            </DialogHeader>
+                            <div className="grid gap-4 py-4">
+                                <div className="grid grid-cols-4 items-center gap-4">
+                                    <Label htmlFor="name" className="text-right">
+                                        Nom
+                                    </Label>
+                                    <Input
+                                        id="name"
+                                        defaultValue="Nom de l'utilisateur"
+                                        className="col-span-3"
+                                    />
+                                </div>
+                                <div className="grid grid-cols-4 items-center gap-4">
+                                    <Label htmlFor="username" className="text-right">
+                                        Email
+                                    </Label>
+                                    <Input
+                                        id="email"
+                                        defaultValue="Email"
+                                        className="col-span-3"
+                                    />
+                                </div>
+                                <div className="grid grid-cols-4 items-center gap-4">
+                                    <Label htmlFor="username" className="text-right">
+                                        Role
+                                    </Label>
+                                    <Input
+                                        id="role"
+                                        defaultValue="Role"
+                                        className="col-span-3"
+                                    />
+                                </div>
+                                <div className="grid grid-cols-4 items-center gap-4">
+                                    <Label htmlFor="username" className="text-right">
+                                        Auto-école
+                                    </Label>
+                                    <Input
+                                        id="school"
+                                        defaultValue="Auto-école"
+                                        className="col-span-3"
+                                    />
+                                </div>
+                                <div className="grid grid-cols-4 items-center gap-4">
+                                    <Label htmlFor="username" className="text-right">
+                                        Mot de passe
+                                    </Label>
+                                    <Input
+                                        id="password"
+                                        defaultValue="Mot de passe"
+                                        className="col-span-3"
+                                    />
+                                </div>
+                                <div className="grid grid-cols-4 items-center gap-4">
+                                    <Label htmlFor="username" className="text-right">
+                                        Confirmation du mot de passe
+                                    </Label>
+                                    <Input
+                                        id="confirm-password"
+                                        defaultValue="Confirmation du mot de passe"
+                                        className="col-span-3"
+                                    />
+                                </div>
+                            </div>
+                            <DialogFooter>
+                                <Button variant='inverseNew' type="submit">Sauvegarder</Button>
+                            </DialogFooter>
+                        </DialogContent>
+                    </Dialog>
 
-                <Button variant='new'>
-                    Nouvel utilisateur
-                    <UserPlus className='w-6 h-6 ml-2' />
-                </Button>
-                    <UserModal />
                     <Button variant='inverseNew'>
                         <LogOut className='w-6 h-6 ml-2' />
                     </Button>
